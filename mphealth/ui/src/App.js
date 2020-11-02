f26bcd0264615da88f597c90f5c8912bd7af3278
Kevin Tu <57199141+kevintu2@users.noreply.github.com>
2020-11-02 13:58:58 -0500

    Changes to frontend
    Added header and logos and favicon

--- mphealth/ui/src/App.js
+++ mphealth/ui/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import ReactCalender from './components/ReactCalender';
+import React, {Component} from 'react';
+import Header from './components/headerfiles/header';
 
-function App() {
+class App extends Component {
+  render(){
   return (
+    
+    
     <div className="App">
+      <Header />
+      <center>
       <ReactCalender />
+      </center>
+      
     </div>
+    
   );
+}
 }
 
 export default App;
