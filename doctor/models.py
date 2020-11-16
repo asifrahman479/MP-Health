3a027aefb93c0ee50fb450956cf75eb4fe16734c
Kevin Tu <57199141+kevintu2@users.noreply.github.com>
2020-11-16 11:02:14 -0500

    Connection to front end
    Connection

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    DoctorName = models.TextField()
+    ClinicID = models.TextField()
+    Email = models.TextField()
+    Phone = models.TextField()
+    Appointments = models.TextField()
+    JobTitle= models.TextField()
