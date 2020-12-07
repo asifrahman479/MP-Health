9edaefd0b8825b4d480420092833c9c84f44ff8f
Kevin Tu <57199141+kevintu2@users.noreply.github.com>
2020-12-07 00:26:27 -0500

    Changes to Model Database
    Testing backend POST request and able to block out slots that are already taken

--- ui/src/components/DoctorView.js
+++ ui/src/components/DoctorView.js
@@ -14,6 +14,7 @@ export default class DoctorView extends SampleBase {
     super(...arguments);
     this.dataManger = new DataManager({
       url: "http://127.0.0.1:8000/api/Appointment/?format=json",
+      crudUrl: "http://127.0.0.1:8000/api/Appointment/",
         // "https://ej2services.syncfusion.com/production/web-services/api/Schedule",
       adaptor: new WebApiAdaptor(),
       crossDomain: true
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from . import models
 from . import serializers
+from rest_framework.decorators import api_view
 
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = models.Doctor.objects.all()
@@ -9,6 +10,15 @@ class DoctorViewSet(viewsets.ModelViewSet):
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = models.Appointment.objects.all()
     serializer_class = serializers.AppointmentSerializer
+
+    @api_view(['POST'])
+    def appointment_list(request):
+        if request.method == 'POST':
+            serializer = AppointmentSerializer(data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status = status.HTTP_201_CREATED)
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = models.Patient.objects.all()
@@ -21,3 +31,11 @@ class DocAvailabilityViewSet(viewsets.ModelViewSet):
 class ClinicViewSet(viewsets.ModelViewSet):
     queryset = models.Clinic.objects.all()
     serializer_class = serializers.ClinicSerializer
+
+# def appointment_list(request):
+#     if request.method == 'POST':
+#         serializer = AppointmentSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status = status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
