e41098e58473ea48257618aeb410fad520126733
Kaela Gobencion <kaelaag@bu.edu>
2020-12-10 17:41:28 -0500

    added an assets view

--- user/views.py
+++ user/views.py
@@ -8,9 +8,6 @@ from .models import Appointment
 from django.shortcuts import redirect
 from django.urls import reverse
 
-
-
-
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = models.Doctor.objects.all()
     serializer_class = serializers.DoctorSerializer
@@ -75,3 +72,14 @@ class ConfirmationView(CreateView):
 
 # def newApt(request):
 #     return render(request, 'apt.html', {"form": CreateAppointment}) 
+
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
