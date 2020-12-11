7ccedb6b22fa1bd5c3f406fa452cbbfbca6f86b3
Asif <rasif@bu.edu>
2020-12-11 00:40:01 -0500

    fixed Heroku Url again

--- src/components/DoctorView.js
+++ src/components/DoctorView.js
@@ -118,7 +118,8 @@ export default class DoctorView extends SampleBase {
 
   async componentDidMount() {
     try {
-      const res = await fetch('http://mphealth.herokuapp.com/api/Appointment'); // fetching the data from api, before the page loaded
+      // fetching the data from api, before the page loaded
+      const res = await fetch('http://mphealth.herokuapp.com/api/Appointment'); 
         const appointmentsList = await res.json();
         this.setState({
           appointmentsList
